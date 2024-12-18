47fad18faa7dafdf74df6b045230085497794c16
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-12-18 16:46:08 +0900

    feat: update

--- src/interaction/interactions.ts
+++ src/interaction/interactions.ts
@@ -1,52 +1,109 @@
 import type {
+	APIApplicationCommandAutocompleteInteraction,
+	APIApplicationCommandInteraction,
 	APIInteraction,
 	APIInteractionResponse,
+	APIMessageComponentInteraction,
+	APIMessageInteraction,
+	APIMessageInteractionMetadata,
+	APIModalSubmitInteraction,
+	APIPingInteraction,
 } from "discord-api-types/v10";
 import {
+	ComponentType,
 	InteractionResponseType,
 	InteractionType,
+	TextInputStyle,
 } from "discord-api-types/v10";
 import { createMiddleware } from "hono/factory";
 import type { Bindings } from "./types";
 
-const RespondToInteraction: Record<
-	InteractionType,
-	(interaction: APIInteraction) => Promise<APIInteractionResponse>
-> = {
-	[InteractionType.Ping]: async (_) => ({
+type InteractionHandler<T extends APIInteraction> = (
+	interaction: T,
+) => Promise<APIInteractionResponse>;
+
+const HandlePing: InteractionHandler<APIPingInteraction> = async () => {
+	return {
 		type: InteractionResponseType.Pong,
-	}),
-	[InteractionType.ModalSubmit]: async (_) => ({
+	};
+};
+
+const HandleCommand: InteractionHandler<
+	APIApplicationCommandInteraction
+> = async (interaction) => {
+	return {
+		type: InteractionResponseType.Modal,
+		data: {
+			title: "モーダル",
+			custom_id: "modal",
+			components: [
+				{
+					type: ComponentType.ActionRow,
+					components: [
+						{
+							type: ComponentType.TextInput,
+							custom_id: "textinput",
+							label: "テキスト",
+							value: "text",
+							style: TextInputStyle.Short,
+						},
+					],
+				},
+			],
+		},
+	} satisfies APIInteractionResponse;
+};
+
+const ModalSubmitHandler: InteractionHandler<
+	APIModalSubmitInteraction
+> = async (interaction) => {
+	return {
 		type: InteractionResponseType.ChannelMessageWithSource,
 		data: {
-			content: "モーダルが送信されました",
+			content: interaction.type.toString(),
 		},
-	}),
-	[InteractionType.MessageComponent]: async (_) => ({
+	};
+};
+
+const MessageComponentHandler: InteractionHandler<
+	APIMessageComponentInteraction
+> = async (interaction) => {
+	return {
 		type: InteractionResponseType.ChannelMessageWithSource,
 		data: {
-			content: "ボタンが押されました",
+			content: interaction.type.toString(),
 		},
-	}),
-	[InteractionType.ApplicationCommand]: async (_) => ({
-		type: InteractionResponseType.ChannelMessageWithSource,
-		data: {
-			content: "コマンドが実行されました",
-		},
-	}),
-	[InteractionType.ApplicationCommandAutocomplete]: async (_) => ({
+	};
+};
+
+const AutoCompleteHandler: InteractionHandler<
+	APIApplicationCommandAutocompleteInteraction
+> = async () => {
+	return {
 		type: InteractionResponseType.ChannelMessageWithSource,
 		data: {
 			content: "オートコンプリートが実行されました",
 		},
-	}),
+	};
+};
+
+const HandleInteraction = (interaction: APIInteraction) => {
+	if (interaction.type === InteractionType.Ping) return HandlePing(interaction);
+	if (interaction.type === InteractionType.ApplicationCommand)
+		return HandleCommand(interaction);
+	if (interaction.type === InteractionType.MessageComponent)
+		return MessageComponentHandler(interaction);
+	if (interaction.type === InteractionType.ApplicationCommandAutocomplete)
+		return AutoCompleteHandler(interaction);
+	if (interaction.type === InteractionType.ModalSubmit)
+		return ModalSubmitHandler(interaction);
 };
 
 export const InteractionHandleMiddleware = createMiddleware<{
 	Bindings: Bindings;
 }>(async (c) => {
 	const interaction: APIInteraction = await c.req.json();
-	const payload = await RespondToInteraction[interaction.type](interaction);
+	const payload = await HandleInteraction(interaction);
 
 	const formData = new FormData();
 	formData.set("payload_json", JSON.stringify(payload));
