b3db57a69d2eaf28c7978dd142f19cd74bb7ba36
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-12-19 02:13:42 +0900

    feat: rpc with swr

--- apps/app-pages/src/app/page.tsx
+++ apps/app-pages/src/app/page.tsx
@@ -1,9 +1,33 @@
 "use client";
 
-import { useHono } from "@/lib/useHono";
+import type { APIType } from "app-workers";
+import { type InferRequestType, hc } from "hono/client";
+import useSWR from "swr";
 
 export default function Home() {
-	const { data } = useHono();
+	const client = hc<APIType>(
+		"https://discord-dashboard-production.ojii3-cloudflare-workers-test.workers.dev",
+	);
+	const $get = client.test.$post;
+
+	const fetcher = (arg: InferRequestType<typeof $get>) => async () => {
+		const res = await $get(arg);
+		return await res.json();
+	};
+
+	const { data, error, isLoading } = useSWR(
+		"api-hello",
+		fetcher({
+			json: {
+				title: "Hello",
+				description: "World",
+			},
+		}),
+	);
+
+	if (isLoading) return <div>Loading...</div>;
+	if (error || !data) return <div>Error</div>;
+
 	return (
 		<div>
 			<h1>{data.title}</h1>
