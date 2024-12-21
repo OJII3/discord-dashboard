14fdfcef6a7c2182f5dbcb777c37e9aa03a425c6
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-12-21 20:14:38 +0900

    add: chakra ui

--- apps/page/src/app/(auth)/dashboard/page.tsx
+++ apps/page/src/app/(auth)/dashboard/page.tsx
@@ -1,6 +1,21 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+	ProgressCircleRing,
+	ProgressCircleRoot,
+} from "@/components/ui/progress-circle";
 import { client } from "@/libs/client";
+import {
+	Center,
+	Container,
+	Grid,
+	GridItem,
+	Heading,
+	List,
+	ProgressCircle,
+	VStack,
+} from "@chakra-ui/react";
 import type { InferRequestType } from "hono/client";
 import useSWR from "swr";
 
@@ -14,17 +29,43 @@ export default function Home() {
 
 	const { data, error, isLoading } = useSWR("/", fetcher({}));
 
-	if (isLoading) return <div>Loading...</div>;
+	if (isLoading)
+		return (
+			<Center minH="vh">
+				<ProgressCircleRoot value={null}>
+					<ProgressCircleRing />
+				</ProgressCircleRoot>
+			</Center>
+		);
 	if (error || !data) return <div>Error</div>;
 
 	return (
-		<div>
-			<div>Roles</div>
-			{data.map((role) => (
-				<div key={role.id}>
-					{role.id}: {role.name}
-				</div>
-			))}
-		</div>
+		<Grid as="main" minH="vh" templateRows="auto 1fr">
+			<GridItem>
+				<header>
+					<Heading>Discord Dashboard</Heading>
+				</header>
+			</GridItem>
+			<Center as={GridItem}>
+				<VStack>
+					<Button
+						variant="surface"
+						onClick={() => {
+							client.register.$get();
+						}}
+					>
+						Re Register Application Commands
+					</Button>
+					<div>Roles</div>
+					<List.Root>
+						{data.map((role) => (
+							<List.Item key={role.id}>
+								{role.id}: {role.name}
+							</List.Item>
+						))}
+					</List.Root>
+				</VStack>
+			</Center>
+		</Grid>
 	);
 }
--- apps/page/src/app/layout.tsx
+++ apps/page/src/app/layout.tsx
@@ -1,6 +1,7 @@
+import { Provider } from "@/components/ui/provider";
+import { Theme } from "@chakra-ui/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -23,11 +24,13 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{children}
+				<Provider>
+					<Theme colorPalette="cyan">{children}</Theme>
+				</Provider>
 			</body>
 		</html>
 	);
