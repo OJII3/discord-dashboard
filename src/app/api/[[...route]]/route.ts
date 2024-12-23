d976f559f30f9ccd5d7294dce3ed8f7492ed52c7
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-12-23 13:26:18 +0900

    fix?

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,6 +1,7 @@
-import { getEnv } from "@/libs/bindings";
+
 import { ping } from "@/libs/commands/ping";
 import { REST } from "@discordjs/rest";
+import { getCloudflareContext } from "@opennextjs/cloudflare";
 import {
 	type APIInteraction,
 	InteractionResponseType,
@@ -9,6 +10,8 @@ import {
 } from "discord-api-types/v10";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+
+let env: CloudflareEnv;
 
 const app = new Hono().basePath("/api");
 
@@ -28,15 +31,16 @@ const app = new Hono().basePath("/api");
 // for Hno RPC
 const route = app
 	.get("/ping", async (c) => c.text("pong", 200))
-	// .get("/register_commands", async (c) => {
-	// 	const rest = new REST({ version: "10" }).setToken(getEnv(c).DISCORD_TOKEN);
-	// 	await rest
-	// 		.post(Routes.applicationCommands(getEnv(c).DISCORD_APPLICATION_ID), {
-	// 			body: [ping.command],
-	// 		})
-	// 		.catch((e) => c.text(`error: ${e}`, 500));
-	// 	return c.text("registered", 200);
-	// });
+	.get("/register_commands", async (c) => {
+		const ctx = await getCloudflareContext();
+		const rest = new REST({ version: "10" }).setToken(ctx.env.DISCORD_TOKEN);
+		await rest
+			.post(Routes.applicationCommands(ctx.env.DISCORD_APPLICATION_ID), {
+				body: [ping.command],
+			})
+			.catch((e) => c.text(`error: ${e}`, 500));
+		return c.text("registered", 200);
+	});
 
 type AppType = typeof route;
 const GET = handle(app);
