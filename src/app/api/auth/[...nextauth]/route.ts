d1746ee566243c5e5bbf9f35757c2bb240ee18ab
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-12-24 02:39:50 +0900

    fix?

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,36 @@
-import { handlers } from "@/auth"; // Referring to the auth.ts we just created
 import { getRequestContext } from "@cloudflare/next-on-pages";
 import type { NextRequest } from "next/server";
-export const { GET, POST } = handlers;
 
 export const runtime = "edge";
 
-// export const GET = (req: NextRequest) => {
-// 	process.env = {
-// 		...process.env,
-// 		...getRequestContext().env,
-// 	};
-// 	return handlers.GET(req);
-// };
-//
-// export const POST = (req: NextRequest) => {
-// 	process.env = {
-// 		...process.env,
-// 		...getRequestContext().env,
-// 	};
-// 	return handlers.POST(req);
-// };
+import NextAuth from "next-auth";
+import Discord from "next-auth/providers/discord";
+
+export const GET = (req: NextRequest) => {
+	const env = getRequestContext().env;
+	const { handlers } = NextAuth({
+		secret: env.AUTH_SECRET,
+		providers: [
+			Discord({
+				clientId: env.AUTH_DISCORD_CLIENT_ID,
+				clientSecret: env.AUTH_DISCORD_CLIENT_SECRET,
+			}),
+		],
+	});
+
+	return handlers.GET(req);
+};
+
+export const POST = (req: NextRequest) => {
+	const env = getRequestContext().env;
+	const { handlers } = NextAuth({
+		secret: env.AUTH_SECRET,
+		providers: [
+			Discord({
+				clientId: env.AUTH_DISCORD_CLIENT_ID,
+				clientSecret: env.AUTH_DISCORD_CLIENT_SECRET,
+			}),
+		],
+	});
+	return handlers.POST(req);
+};
