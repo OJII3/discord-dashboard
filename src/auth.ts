429ccd0ad67584701dfaac233a45fdbd9b798b87
ojii3 <84656786+OJII3@users.noreply.github.com>
2024-12-24 02:36:25 +0900

    fix?

--- src/auth.ts
+++ src/auth.ts
@@ -2,14 +2,12 @@ import { getRequestContext } from "@cloudflare/next-on-pages";
 import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
 
-const env = getRequestContext().env;
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	secret: env.AUTH_SECRET,
+	secret: getRequestContext().env.AUTH_SECRET,
 	providers: [
 		Discord({
-			clientId: env.AUTH_DISCORD_CLIENT_ID,
-			clientSecret: env.AUTH_DISCORD_CLIENT_SECRET,
+			clientId: getRequestContext().env.AUTH_DISCORD_CLIENT_ID,
+			clientSecret: getRequestContext().env.AUTH_DISCORD_CLIENT_SECRET,
 		}),
 	],
 });
